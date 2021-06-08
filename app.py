40ddedf822fbea19a3e67842649ea8dfee8ab489
logicthinkedu <62051098+logicthinkedu@users.noreply.github.com>
2021-06-08 11:56:14 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ nlp = spacy.load("en_core_web_md")
 with open(r'dataset/word2idx.json', "r") as f:
     word2idx = json.load(f)
 
-textCNN_model = load_model(os.path.join("model/","my_JD_presetation_model.h5"))
+textCNN_model = load_model(os.path.join("./model/","my_JD_presetation_model.h5"))
 layer_output = textCNN_model.get_layer('concatenate').output
 intermediate_model = tf.keras.models.Model(inputs=textCNN_model.input,outputs=layer_output)
 
@@ -178,7 +178,7 @@ for i in range(0,len(intermediate_prediction)):
     JD_Vector_List.append( intermediate_prediction[i][0][0] )
     
     
-tfidf = TfidfModel.load( os.path.join("tfidf_model/","my_corpora.tfidf_model") )
+tfidf = TfidfModel.load( os.path.join("./tfidf_model/","my_corpora.tfidf_model") )
 
 def get_Sentence_Vector( wordID_list ):
     word_count_list = Counter([w for w in wordID_list if w != 0]).most_common()
